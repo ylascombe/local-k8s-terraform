106ce27e07483de9d4d88baa06ccfb304e066101
Yohan Lascombe <yohan@mac.home>
2025-01-20 21:52:11 +0100

    chore bump argocd helm chart version

--- modules/argocd/main.tf
+++ modules/argocd/main.tf
@@ -4,7 +4,7 @@ resource "helm_release" "argocd_system" {
   name       = "argo-cd"
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
-  version    = "5.19.12"
+  version    = "7.7.16"
   timeout    = 3000
 
   create_namespace = true
